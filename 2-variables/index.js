cbea301296ff1af05a28f42dd879aaadbbcb4707
vishal2537 <vishal.prajapti21b@iiitg.ac.in>
2023-06-25 20:24:54 +0530

    variable

---
+++ 2-variables/index.js
@@ -0,0 +1,25 @@
+// variable --
+// A variable is a containers that stores a value.
+// The value of a javascript variable can be changes during the exection of a pragram.
+// variable is case sensitive.
+
+// var vs let -----
+// 1-> var is globally scoped while let and const are block scoped
+// 2-> var can be updated and redeclared within its scope
+// 3-> let can be updated but not redeclared
+// 4-> const can neither be updated nor be redeclared
+// 5-> var variablws are initialised with indefined whereas let and const variable are not initilised
+// 6-> const must be initilised during declaration inlike let and var
+
+//  primitive datatype and objects --
+// are a set of basic datatype in js
+// object is a non-primitve datatype in javascript
+
+// 7 primitive datatype in js
+// null,number,string,symbol,undefined,boolean,BigInt
+
+console.log("variable")
+let a = 67
+a ="hvjkck"
+console.log(a)
+
---
+++ 2-variables/letvarconst.js
@@ -0,0 +1,15 @@
+console.log("welcome to js-tut")
+var a = 454;
+// var a = "p"
+let b = "afagh"
+const name = "vishal"
+// b = 4
+
+const vis = 0
+let c = null
+let d = undefined
+{
+    let b = "random"
+    console.log(b)
+}
+console.log(b)
