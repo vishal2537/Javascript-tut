43a702bb08c391fc898b827ace375ecc6562a223
vishal2537 <vishal.prajapti21b@iiitg.ac.in>
2023-07-03 21:08:03 +0530

    string

---
+++ 8-string/index.js
@@ -0,0 +1,21 @@
+let name = "vishal"
+console.log(name)
+console.log(name.length)
+
+let fr = 'Devil'
+console.log(fr)
+
+// Template literals
+let boy1 = "Pramod"
+let boy2 = "Nikhil"
+// Nikhil is a friend of Pramod
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
+// / Escape Sequence Characters
+// let fruit = 'Bana\'na'
+// console.log(fruit)
+let fruit = "Bana\"na"
+console.log(fruit)
+
+
