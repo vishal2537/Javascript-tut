e7e4d6ef2d4a971c27680e92e50c73e94d693c7e
vishal2537 <vishal.prajapti21b@iiitg.ac.in>
2023-07-03 19:10:53 +0530

    function

---
+++ 7-function/index.js
@@ -0,0 +1,24 @@
+const hello = () => {
+    console.log("hey how are you")
+    return "hi";
+}
+
+function average(x, y)
+{
+    return (x + y) / 2
+}
+
+const sum = (p, q) => {
+    console.log("done")
+    return (p + q)
+}
+
+
+let a = 1;
+let b = 2;
+let c = 3;
+let asn = hello();
+console.log(asn)
+console.log(average(a, b))
+console.log(sum(a, c))
+
